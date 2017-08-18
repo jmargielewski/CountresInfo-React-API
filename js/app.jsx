f91dc1bfd5623db0dea0d72ed436d0c703f9d052
Jakub Margielewski <kuba.margielewski@gmail.com>
2017-08-18 22:51:51 +0200

    ex component

--- js/app.jsx
+++ js/app.jsx
@@ -3,8 +3,38 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', function(){
 
+    class Header extends React.Component{
+        render(){
+            return <header>header...</header>
+        }
+    }
+
+    class Main extends React.Component{
+        render(){
+            return <main>main...</main>
+        }
+    }
+
+    class Footer extends React.Component{
+        render(){
+            return <footer>footer...</footer>
+        }
+    }
+
+    class App extends React.Component{
+        render(){
+            return(
+                <div>
+                    <Header/>
+                    <Main/>
+                    <Footer/>
+                </div>
+            )
+        }
+    }
+
     ReactDOM.render(
-        <h1>Hello, World!</h1>,
+        <App/>,
         document.getElementById('app')
     );
 });
